984309ac190aac7243b8362a5a4191bf3aa5ff50
wangshuo <wangshuo@benmu-health.com>
2018-11-12 01:44:45 +0800

    add thread class

---
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/InitDataThread.java
@@ -0,0 +1,33 @@
+package com.alibabacloud.polar_race.engine.common;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author wangshuo
+ * @version 2018-11-12
+ */
+public class InitDataThread extends Thread {
+
+    private int fileNo;
+    private String path;
+    private Data[] datas;
+    private CountDownLatch downLatch;
+
+    public InitDataThread(int fileNo, String path, Data[] datas, CountDownLatch downLatch) {
+        this.fileNo = fileNo;
+        this.path = path;
+        this.datas = datas;
+        this.downLatch = downLatch;
+    }
+
+    @Override
+    public void run() {
+        try {
+            datas[fileNo] = new Data(path, fileNo);
+            downLatch.countDown();
+        } catch (IOException e) {
+            System.out.println("init data file IO exception!!!");
+        }
+    }
+}
