d8cff6d31cb0a5f0bcc0b91ee80c8f00957bd2c4
wangshuo <wangshuo@benmu-health.com>
2018-11-12 00:20:27 +0800

    access 32 thread read

--- engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/Constant.java
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/Constant.java
@@ -12,7 +12,7 @@ public interface Constant {
 
     int INIT_MAP_CAP = TOTAL_KV_COUNT / DATA_FILE_COUNT; //初始索引 map 的容量
 
-    byte ACCESS_FILE_COUNT = 4; //访问文件的channel数
+    byte ACCESS_FILE_COUNT = 32; //访问文件的channel数
 
     byte OFFSET_SIZE = 4;
 
