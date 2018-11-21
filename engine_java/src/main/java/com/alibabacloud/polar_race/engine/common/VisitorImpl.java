9b40662e9e2d8e3c45d52d7b167f7d58e2c3e5cb
xiudongxu <xiudongxu@163.com>
2018-11-21 23:57:06 +0800

    test range

---
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/VisitorImpl.java
@@ -0,0 +1,13 @@
+package com.alibabacloud.polar_race.engine.common;
+
+/**
+ * @author dongxu.xiu
+ * @since 2018-11-21 下午8:10
+ */
+public class VisitorImpl extends AbstractVisitor {
+
+    @Override
+    public void visit(byte[] key, byte[] value) {
+        System.out.println();
+    }
+}
